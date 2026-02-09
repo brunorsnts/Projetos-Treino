26740b32abfedfde8cfe8bbc02f260cec6cfba30
brunorsnts <santos_2002@outlook.com.br>
2026-02-09 17:31:49 -0300

    feat: create PrincipalComListas class to practice list manipulation

---
+++ 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,39 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrincipalComListas {
+
+    public static void main(String[] args) {
+
+        List<Titulo> titulos = new ArrayList<>();
+
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        Filme filmeDoBruno = new Filme("Titanic", 2000);
+        Serie lost = new Serie("Lost", 2000);
+        meuFilme.avalia(5);
+        outroFilme.avalia(8);
+        filmeDoBruno.avalia(9);
+
+        titulos.add(meuFilme);
+        titulos.add(outroFilme);
+        titulos.add(filmeDoBruno);
+        titulos.add(lost);
+
+        for (Titulo item : titulos) {
+
+            System.out.println(item);
+
+            if (item instanceof Filme titulo) {
+                System.out.println("Classificação: " + titulo.getClassificacao());
+            }
+        }
+    }
+
+}
