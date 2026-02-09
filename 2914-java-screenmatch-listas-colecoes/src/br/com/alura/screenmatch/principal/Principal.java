a1763ae7cd682710c687d58174be9937680133e6
brunorsnts <santos_2002@outlook.com.br>
2026-02-09 08:50:16 -0300

    feat: add constructors to Titulo, Filme and Serie classes

--- 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/principal/Principal.java
+++ 2914-java-screenmatch-listas-colecoes/src/br/com/alura/screenmatch/principal/Principal.java
@@ -1,3 +1,5 @@
+package br.com.alura.screenmatch.principal;
+
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
 import br.com.alura.screenmatch.modelos.Episodio;
@@ -5,7 +7,6 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Principal {
@@ -13,9 +14,7 @@ public class Principal {
 
         List<Filme> filmes = new ArrayList<>();
 
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefão");
-        meuFilme.setAnoDeLancamento(1970);
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
@@ -29,18 +28,14 @@ public class Principal {
         //meuFilme.totalDeAvaliacoes = 1;
         //System.out.println(meuFilme.pegaMedia());
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(2000);
+        Serie lost = new Serie("Lost", 2000);
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        outroFilme.setNome("Avatar");
-        outroFilme.setAnoDeLancamento(2023);
+        Filme outroFilme = new Filme("Avatar", 2023);
         outroFilme.setDuracaoEmMinutos(200);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
@@ -58,9 +53,7 @@ public class Principal {
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
 
-        Filme filmeDoBruno = new Filme();
-        filmeDoBruno.setNome("Titanic");
-        filmeDoBruno.setAnoDeLancamento(2000);
+        Filme filmeDoBruno = new Filme("Titanic", 2000);
 
         filmes.add(meuFilme);
         filmes.add(outroFilme);
